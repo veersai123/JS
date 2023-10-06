f92b8b460d775b23e5355118872e849e61c34094
Deepak Saini <ds7023500@gmail.com>
2023-10-07 02:19:47 +0530

    second commit

--- project1/App.js
+++ project1/App.js
@@ -1,15 +1,39 @@
+// var istatus=document.querySelector("h5")
+
+// var addFriend=document.querySelector("#add")
+// var removeFriend=document.querySelector("#remove")
+
+// addFriend.addEventListener("click",function()
+// {
+//     istatus.innerHTML="React"
+//     istatus.style.color="green"
+// })
+
+// removeFriend.addEventListener("click",function(){
+//     istatus.innerHTML="JavaScript"
+//     istatus.style.color="red"
+// })
+
+// ========================================================================
+
 var istatus=document.querySelector("h5")
 
-var addFriend=document.querySelector("#add")
-var removeFriend=document.querySelector("#remove")
-
+var addFriend=document.querySelector("#btn")
+var check = 0
 addFriend.addEventListener("click",function()
 {
-    istatus.innerHTML="React"
-    istatus.style.color="green"
+    if(check==0)
+    {
+        istatus.innerHTML="React"
+        istatus.style.color="green"
+        btn.innerHTML="back"
+        check=1
+    }
+    else{
+        istatus.innerHTML="Java"
+        istatus.style.color="red"
+        btn.innerHTML="proceed"
+        check=0
+    }
 })
 
-removeFriend.addEventListener("click",function(){
-    istatus.innerHTML="JavaScript"
-    istatus.style.color="red"
-})
